873ac60f14d064c580c24217bdebd7a803caa983
kaiquesilvadev <j.kaique1570@gmail.com>
2023-06-15 17:16:26 -0300

    project init

---
+++ src/componentes/HomPages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '../card/';
+import "./style.css"
+
+
+export default function HomePage() {
+  return (
+    <div className='home-page'>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
